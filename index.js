836d082de160a4a63d9ea6f9c2c2767c5f91793c
Ali Raihan Nashrullah <aliraihann@gmail.com>
2023-11-10 19:04:59 +0700

    init

--- index.js
+++ index.js
@@ -20,13 +20,94 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// app.get("/api/hello", function (req, res) {
+//   res.json({greeting: 'hello API'});
+// });
+
+app.get("/api", function (req, res) {
+  res.json({
+    unix: Math.floor(new Date()).valueOf(),
+    utc: new Date().toUTCString(),
+  });
 });
 
+app.get("/api/:date", function (req, res) {
+  const date = req.params.date;
+  if (date != undefined) {
+    let regex = /^[0-9]+$/
+    if (regex.test(date)) {
+      let unixReqParse = parseInt(date, 10);
+      let unixDate = new Date(unixReqParse);
+      if (isNaN(unixDate.getTime())) {
+        res.json({
+              error: "Invalid Date",
+            });
+            return;
+      }
+      let unixTime = unixDate.getTime();
+      let unixUtcTime = unixDate.toUTCString();
+      res.json({
+        unix: unixTime,
+        utc: unixUtcTime,
+      });
+    } else {
+      let utcDate = new Date(date);
+      if (isNaN(utcDate.getTime())) {
+        res.json({
+              error: "Invalid Date",
+            });
+            return;
+      }
+      let utcUnixTime = utcDate.getTime();
+      let utcTime = utcDate.toUTCString();
+      res.json({
+        unix: utcUnixTime,
+        utc: utcTime,
+      });
+    }
+  }
+});
+  
 
+// app.get("/api/:date", function (req, res) {
+//   let date_string = req.params.date;
+//   let unixTime = parseInt(date_string);
+//   if (date_string != undefined) {
+//     if (date_string.match("-") != null) {
+//       res.json({
+//         unix: Math.floor(new Date(date_string)).valueOf(),
+//         utc: new Date(date_string).toUTCString(),
+//       });
+//     } else {
+//       // let date_string = parseInt(req.params.date);
+//       let unixDate = parseInt(date_string, 10)
+//       let date = new Date(unixDate);
+//       res.json({
+//         unix: date.getTime(),
+//         utc: date.toUTCString(),
+//       });
+//     }      
+//   }
+// });
+//==============
+  // }
+  //   res.json({ 
+  //     error : "Invalid Date" 
+  //   })
+
+  // } else {
+  //   res.json({
+  //     unix: new Date(date_string).getTime(),
+  //     utc: new Date(date_string).toUTCString(),
+  // })
+
+// };
+
+// unix: Math.floor((new Date(date_string)).getTime()),
+// utc: new Date(date_string).toUTCString(),
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const port = process.env.PORT || 3000;
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
